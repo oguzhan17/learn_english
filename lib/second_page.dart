bd50f60c1ba05dc8ce6248b66e6e21f1d3464826
oguzhan17 <oguzhancevher61@gmail.com>
2021-01-04 01:35:54 +0300

    Speech

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -2,8 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class SecondPage extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
   String passData;
   SecondPage({Key key, @required this.passData}) : super(key: key);
 
@@ -18,90 +20,107 @@ class SecondPage extends StatelessWidget {
             onPressed: () => Navigator.pop(context),
           ),
         ),
-        body:  MyHomePage(passData: passData,),
+        body: MyHomePage(
+          passData: passData,
+          flutterTts: flutterTts,
+        ),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  FlutterTts flutterTts;
   String passData;
-  MyHomePage({Key key, @required this.passData}) : super(key: key);
+  MyHomePage({Key key, @required this.passData, @required this.flutterTts})
+      : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState(passData);
+  _MyHomePageState createState() => _MyHomePageState(passData, flutterTts);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   String passData;
-  _MyHomePageState(this.passData);
+  FlutterTts flutterTts;
+  _MyHomePageState(this.passData, this.flutterTts);
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection(passData)
-              .orderBy('name', descending: false)
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              return buildBody(context, snapshot.data.docs);
-            }
+        stream: FirebaseFirestore.instance
+            .collection(passData)
+            .orderBy('name', descending: false)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return buildBody(context, snapshot.data.docs);
           }
-    );
+        });
   }
 
   Widget buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+    Future _speak(String speakText) async {
+      await flutterTts.speak(speakText);
+    }
 
     return GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            children: List.generate(
-              snapshot.length,
-              (index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: FlipCard(
-                      front: Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(0.0,5.0),
-                            ),
-                          ],
-                            borderRadius: BorderRadius.circular(20.0),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    GetGrid.fromSnapshot(snapshot[index])
-                                        .photo),
-                                fit: BoxFit.cover)),
-                      ),
-                      direction: FlipDirection.HORIZONTAL, // default
-                      back: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    GetGrid.fromSnapshot(snapshot[index])
-                                        .photo),
-                                colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
-                                fit: BoxFit.cover)),
-                        child: Center(
-                            child: Text(
-                                GetGrid.fromSnapshot(snapshot[index]).name,style: TextStyle(fontSize: 40,color: Colors.black),),),
-                      )),
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      crossAxisSpacing: 10.0,
+      children: List.generate(
+        snapshot.length,
+        (index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: FlipCard(
+                front: Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 5.0),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              GetGrid.fromSnapshot(snapshot[index]).photo),
+                          fit: BoxFit.cover)),
                 ),
-              ),
-            ),
-          );
-
+                direction: FlipDirection.HORIZONTAL, // default
+                back: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              GetGrid.fromSnapshot(snapshot[index]).photo),
+                          colorFilter: new ColorFilter.mode(
+                              Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                          fit: BoxFit.cover)),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          GetGrid.fromSnapshot(snapshot[index]).name,
+                          style: TextStyle(fontSize: 40, color: Colors.black),
+                        ),
+                        RaisedButton(
+                          child: Text('play'),
+                          onPressed: () => _speak(
+                              GetGrid.fromSnapshot(snapshot[index]).name),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          ),
+        ),
+      ),
+    );
   }
 }
 
